db8e32b1b9c5effed797d681f51ba40f17f489ee
nip403 <41174986+nip403@users.noreply.github.com>
2019-09-25 11:57:14 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,61 +1,75 @@
-import pygame
-import random
-import sys
+from mGameEngine import Animator
+import random as rn
+import itertools
 
-_overlap = True
+def create_maze(length, width, sq=None, animate=None):
+    return Maze(length, width, sq=sq, animate=animate).generate()
 
-class Board:
-    def __init__(self,surf,resolution):
-        self.resolution = resolution
-        self.surf = surf
-        self.board = [[0 for _ in range(resolution[1])] for _ in range(resolution[0])]
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-        self.square_size = int(self.surf.get_width()/self.resolution[0])
-        
-    def generate(self,mode=None,density=1):
-        # 0 = random, density (specific to mode=0) used for: randint(0,density) == 0
-        # 1 = input
+        self.visited = False
+        self.walls = {
+            "U": True,
+            "D": True,
+            "L": True,
+            "R": True
+        }
 
-        if mode is None:
-            return
+class Maze:
+    def __init__(self, length, width, animate=False, sq=None): # implement options for different algorithms
+        self.length = length
+        self.width = width
 
-        if mode == 0:
-            self.board = [[1 if not random.randint(0,density) else 0 for _ in range(self.resolution[1])] for _ in range(self.resolution[0])]
-        elif mode == 1:
-            self.input_config()
+        self.maze = [[Cell(x, y) for x in range(self.width)] for y in range(self.length)]
 
-    def input_config(self):
-        self.board = [[0 for _ in range(self.resolution[1])] for _ in range(self.resolution[0])]
-        clock = pygame.time.Clock()
+        self.animate = bool(animate)
+        if self.animate:
+            self.graphics = Animator(self.width, self.length, sq)
 
-        while True:
-            clock.tick(60)
-            
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+    def any_unvisited(self):
+        return any(any(not c.visited for c in row) for row in self.maze)
 
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    self.board[int(pygame.mouse.get_pos()[1]/self.square_size)][int(pygame.mouse.get_pos()[0]/self.square_size)] = 1 - self.board[int(pygame.mouse.get_pos()[1]/self.square_size)][int(pygame.mouse.get_pos()[0]/self.square_size)]
+    def get_neighbours(self, current):
+        #neighbours = list(itertools.combinations_with_replacement([-1,0,1],2))
+        neighbours = [(-1,0),(0,-1),(1,0),(0,1)]
+        return list(filter(lambda i: (i[0] or i[1]) and (0 <= i[0]+current[0] < self.width and 0 <= i[1]+current[1] < self.length) and (not self.maze[i[1]+current[1]][i[0]+current[0]].visited), set(neighbours + [n[::-1] for n in neighbours])))
 
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                    return
+    def remove_wall(self, a, b):
+        if a[0] > b[0]:
+            self.maze[a[1]][a[0]].walls["L"] = False
+            self.maze[b[1]][b[0]].walls["R"] = False
+        elif a[0] < b[0]:
+            self.maze[a[1]][a[0]].walls["R"] = False
+            self.maze[b[1]][b[0]].walls["L"] = False
+        if a[1] > b[1]:
+            self.maze[a[1]][a[0]].walls["U"] = False
+            self.maze[b[1]][b[0]].walls["D"] = False
+        elif a[1] < b[1]:
+            self.maze[a[1]][a[0]].walls["D"] = False
+            self.maze[b[1]][b[0]].walls["U"] = False
 
-            if pygame.mouse.get_pressed()[2]:
-                self.board[int(pygame.mouse.get_pos()[1]/self.square_size)][int(pygame.mouse.get_pos()[0]/self.square_size)] = 1 - self.board[int(pygame.mouse.get_pos()[1]/self.square_size)][int(pygame.mouse.get_pos()[0]/self.square_size)]
+    def generate(self): # recursive backtracker
+        stack = []
+        cell = [0, 0]
 
-            self.draw()
+        while self.any_unvisited():
+            self.maze[cell[1]][cell[0]].visited = True
+            neighbours = self.get_neighbours(cell)
 
-            pygame.display.flip()
+            if len(neighbours):
+                neighbour = rn.choice(neighbours)
+                neighbour = [cell[i] + neighbour[i] for i in range(2)]
+                stack.append(cell)
+                self.remove_wall(cell, neighbour)
+                cell = neighbour[:]
+                
+            elif len(stack):
+                cell = stack.pop()
 
-    def neighbours(self,pos):
-        return sum(self.board[neighbour[1]][neighbour[0]] for neighbour in [p for p in [[pos[0]-1,pos[1]-1],[pos[0],pos[1]-1],[pos[0]+1,pos[1]-1],[pos[0]-1,pos[1]],[pos[0]+1,pos[1]],[pos[0]-1,pos[1]+1],[pos[0],pos[1]+1],[pos[0]+1,pos[1]+1]] if all(i >= 0 and p[q] < [len(self.board[0]),len(self.board)][q] for q,i in enumerate(p))]) if not _overlap else sum(self.board[neighbour[1]][neighbour[0]] for neighbour in [[p[0] if p[0] < len(self.board[0]) else p[0]-len(self.board[0]),p[1] if p[1] < len(self.board) else p[1]-len(self.board)] for p in [[pos[0]-1,pos[1]-1],[pos[0],pos[1]-1],[pos[0]+1,pos[1]-1],[pos[0]-1,pos[1]],[pos[0]+1,pos[1]],[pos[0]-1,pos[1]+1],[pos[0],pos[1]+1],[pos[0]+1,pos[1]+1]]])
+            if self.animate:
+                self.graphics.draw(self.maze, cell)
 
-    def advance(self):
-        self.board = [[1 if ((self.board[y][x] and self.neighbours([x,y]) in [2,3]) or (not self.board[y][x] and self.neighbours([x,y]) == 3)) else 0 for x in range(len(self.board[0]))] for y in range(len(self.board))]
-
-    def draw(self):
-        for y,row in enumerate(self.board):
-            for x,item in enumerate(row):
-                pygame.draw.rect(self.surf,[255]*3 if item else [0]*3,[x*self.square_size,y*self.square_size,self.square_size,self.square_size],0)
+        return self.maze
